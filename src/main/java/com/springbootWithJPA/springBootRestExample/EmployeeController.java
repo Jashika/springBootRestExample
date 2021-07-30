6758df59a725fc6a76e2cdd59eb4f338af8930fc
Jashika <jashikavasu@gmail.com>
2021-07-30 16:59:14 +0530

    Minor changes

--- src/main/java/com/springbootWithJPA/springBootRestExample/EmployeeController.java
+++ src/main/java/com/springbootWithJPA/springBootRestExample/EmployeeController.java
@@ -1,35 +1,59 @@
 package com.springbootWithJPA.springBootRestExample;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeController {
 
-        @Autowired
-        private EmployeeRepository employeeRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
-        @PostMapping("/employee")
-        public Employee createEmployee(@RequestBody Employee employee) {
-            return this.employeeRepository.save(employee);
-        }
-
-        @GetMapping("/employee/{id}")
-        public Optional<Employee> getEmployee(@PathVariable(value = "id") Integer id) {
-            return employeeRepository.findById(id);
-        }
-
-        @PutMapping("/employee/{id}")
-        public Employee updateEmployee(@PathVariable(value = "id") Integer id,@RequestBody Employee employee ){
-           Employee emp= employeeRepository.getById(id);
-            emp.setFirstName(employee.getFirstName());
-            emp.setLastName(employee.getLastName());
-            emp.setEmailId(employee.getEmailId());
-            return employeeRepository.save(emp);
-        }
-
+    @PostMapping("/employee")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
+
+    @GetMapping("/employee/{id}")
+    public Optional<Employee> getEmployee(@PathVariable(value = "id") Integer id) {
+        return employeeRepository.findById(id);
+    }
+
+    @PutMapping("/employee/{id}")
+    public Employee updateEmployee(@PathVariable(value = "id") Integer id, @RequestBody Employee employee) {
+        Employee emp = employeeRepository.getById(id);
+        emp.setFirstName(employee.getFirstName());
+        emp.setLastName(employee.getLastName());
+        emp.setEmailId(employee.getEmailId());
+        return employeeRepository.save(emp);
+    }
+
+    @GetMapping("/employee/allEmployees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @DeleteMapping("/employee/{id}")
+    public ResponseEntity<HttpStatus> deleteEmployee(@PathVariable Integer id) {
+        try {
+            employeeRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
 
